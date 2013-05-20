68cd4728adeac5a6a75c886a4b0f20a19aad744e
sdas <>
2013-05-20 06:31:25 +0000

    processBTagging_Data.sh

---
+++ HbbHbb/Analysis/MMMM/processBTagging_Signal.sh
@@ -0,0 +1,7 @@
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX300ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX400ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX500ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX600ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX700ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX800ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "MMMM", "PUWeight.root", 0)' 
+say "Signal Monte Carlo with pile up done"
